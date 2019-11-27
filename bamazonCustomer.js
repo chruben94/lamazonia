3f2e316d7b1f1b590d145dc5a2a36e68410f4413
Ruben Chavez <chruben94@gmail.com>
2019-11-26 23:05:48 -0800

    connection confirmed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,19 +2,19 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-        host:"localHost",
-        port: 8282,
+        host:"localhost",
+        port: 3306,
         user: "root",
         password: "password",
         database: "bamazon_db"
 });
 
 connection.connect(function(err){
-    console.log("connected as id: "+connection.ThreadId);
+    console.log("connected as id: "+ connection.threadId);
+    start()
+});
 
-})
-
-function start(){
+function start() {
 
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
@@ -63,12 +63,7 @@ function start(){
 
                 });
             }
-        }       
-
-​
-​
-
-
+        };
 //The app should then prompt users with two messages.
 
 //The first should ask them the ID of the product they would like to buy.
