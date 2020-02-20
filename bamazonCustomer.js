0386ab66db217907ee86ed540815ce7642f2a8d5
Ruben Chavez <chruben94@gmail.com>
2020-02-19 16:07:10 -0800

    should be working but there is a bug

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,77 +1,133 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-        host:"localhost",
-        port: 3306,
-        user: "root",
-        password: "password",
-        database: "bamazon_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "bamazon_db"
 });
 
-connection.connect(function(err){
-    console.log("connected as id: "+ connection.threadId);
-    start()
+connection.connect(function (err) {
+  if (err) throw err;
+
+  start();
 });
 
 function start() {
+  inquirer
+    .prompt({
+      name: "display",
+      type: "list",
+      message: "Welcome to Bamazon! Would you like to browse our products?",
+      choices: ["Yes!", "No"]
+    })
+    .then(function (answer) {
+      if (answer.display === "Yes!") {
+        display();
+      } else if (answer.display === "No") {
+        connection.end();
+      }
+    });
+}
 
-    connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
-        for (var i = 0; i < results.length; i++) {
-            console.log("---------------")
-            console.log("ID number: " + results[i].id)
-            console.log("Product Name: " + results[i].name)
-            console.log("Price: $" + results[i].price)
+function doAgain() {
+  inquirer
+    .prompt({
+      name: "none",
+      type: "list",
+      message: "Would you like to browse other products?",
+      choices: ["Yes!", "No"]
+    })
+    .then(function (answer) {
+      if (answer.none === "Yes!") {
+        display();
+      } else if (answer.none === "No") {
+        connection.end();
+      }
+    });
+}
+
+function display() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+    for (var i = 0; i < results.length; i++) {
+      console.log("-------------------");
+      console.log("ID Number: " + results[i].id);
+      console.log("Product Name: " + results[i].product_name);
+      console.log("Price: $" + results[i].price);
+    }
+    input();
+  });
+
+  function input() {
+    inquirer
+      .prompt([
+        {
+          name: "idchoice",
+          type: "input",
+          message: "If you would like to purchase a product, please input the product's ID number:"
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How many would you like to buy?"
         }
-        input();
-
-        })
-        function input() {
-            inquirer
-                .prompt([
-                    {
-                        name: "chooseid",
-                        type: "input",
-                        message: "Please input the ID of the product you would like to purchase:"
-                    }
-                    ,
-                    {
-                        name: "quantity",
-                        type: "input",
-                        message: "How many would you like to purchase?"
-                    }
-                ])
-                .then(function(answer) {
-                    connection.query("SELECT * FROM products WHERE id = ?", 
-                    [
-                        parseInt(answer.chooseid)
-                    ],
-                      function(err, res) {
-                        if (err) throw err;
-                        var quantity = parseInt(res[0].stock_quantity)
-                        // console.log(quantity)
-                        if (quantity <= parseInt(answer.quantity)) {
-                            console.log("Insufficient quantity!")
-                        }
-                        else if (quantity >= parseInt(res[0].stock_quantity)) {
-                            var total = parseInt(res[0].price) * parseInt(answer.quantity)
-                            console.log("Your order has been confirmed! Your total was $" + total + ". Thank you for shopping with Bamazon.")
-                        }
-                      }
-                    );
-
-                });
+      ])
+      .then(function (answer) {
+        connection.query(
+          "SELECT * FROM products WHERE id = ?",
+          [parseInt(answer.idchoice)],
+          function (err, res) {
+            if (err) throw err;
+            var quantity = parseInt(res[0].stock_quantity);
+            if (quantity <= parseInt(answer.quantity)) {
+              console.log("Insufficient quantity!");
+              doAgain();
+            } else if (quantity >= parseInt(res[0].stock_quantity)) {
+              var product = res[0].product_name;
+              var chosenID = parseInt(res[0].id);
+              var totalPrice =
+                parseInt(res[0].price) * parseInt(answer.quantity);
+              var updatedQuantity = (parseInt(res[0].stock_quantity) = parseInt(
+                answer.quantity
+              ));
+              connection.query(
+                "Update products Set ? Where ?",
+                [
+                  {
+                    stock_quantity: updatedQuantity
+                  },
+                  {
+                    id: chosenID
+                  }
+                ],
+                function (err, results) {
+                  if (err) throw err;
+                  console.log("-------------");
+                  console.log("Your purchase was succesful!");
+                  console.log("-------------");
+                  console.log("Your total was $" + totalPrice + ".");
+                  console.log("Thank you for shopping with us");
+                  console.log("-------------");
+                }
+              );
+              connection.query(
+                "Select * FROM products WHERE id = ?",
+                [parseInt(answer.idchoice)],
+                function (err, res) {
+                  if (err) throw err;
+                  console.log(product + "s left: " + res[0].stock_quantity);
+                  console.log("----------");
+                  console.log("----------");
+                  console.log("----------");
+                  doAgain();
+                }
+              );
             }
-        };
-//The app should then prompt users with two messages.
-
-//The first should ask them the ID of the product they would like to buy.
-//The second message should ask how many units of the product they would like to buy.
-//Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-//If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-//However, if your store does have enough of the product, you should fulfill the customer's order.
-
-//This means updating the SQL database to reflect the remaining quantity.
-//Once the update goes through, show the customer the total cost of their purchase.
+          }
+        );
+      });
+  }
+}
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -1,13 +1,27 @@
 DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
-use bamazon_db;
 
-CREATE TABLE products (
+USE bamazon_db;
 
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(45) NULL,
-department_name VARCHAR(45) NULL,
-price integer default 0,
-stock_quantity integer default 0,
-PRIMARY KEY (id)
+CREATE TABLE products(
+    id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(45) NOT NULL,
+    price INT, 
+    stock_quantity INT,
+    PRIMARY KEY(id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+
+VALUE ("toaster", "kitchen", 20, 10),
+("iphone", "electronics", 700, 10),
+("table", "furniture", 70, 10),
+("glasses", "accessories", 130, 10),
+("scarf", "clothes", 10, 10),
+("appleWatch", "electronics", 300, 10),
+("plates", "kitchen", 20, 10),
+("sweater", "clothes", 40, 10),
+("Drive", "Books", 30, 10),
+("handcuffs", "accessories", 20, 10);
+
